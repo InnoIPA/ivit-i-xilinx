8d60d488bfc848522063557c9f8106d8f2aae4fd
HuiJu1218 <ushueiru4567mail.com>
2022-07-19 19:30:33 -0700

    feat: add requirement

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+# Utility
+pip3 install gdown wget colorlog
+
+# Pycocotools
+sudo dnf install python3-matplotlib
+pip3 install pycocotools
+
+# for Flask
+pip3 install flask flask-socketio==5.1.2 flask-cors flasgger gunicorn==20.1.0 eventlet==0.30.2 \
+python-engineio==4.3.2 python-socketio==5.6.0
