494155632965317aabcee4a779eea56528ad0437
HuiJu1218 <ushueiru4567mail.com>
2022-07-19 23:21:29 -0700

    feat: define key and add process of rescaling bounding box

--- ivit_i/cls/classification.py
+++ ivit_i/cls/classification.py
@@ -1,52 +1,102 @@
 import sys, os, copy, logging, time
 import numpy as np
 sys.path.append("..")
-from ivit_i.common.common import XMODEL, runDPU_
-from ivit_i.common.config_key_list import *
+from ivit_i.common.common import XMODEL, IVIT_MODEL, runDPU_
 from ivit_i.common.pre_post_proc import preprocess_fn
+from ivit_i.utils.parser import load_txt
+from ivit_i.utils.err_handler import handle_exception
+
+# Define Key
+TAG         = "tag"
+FRAMEWORK   = "framework"
+MODEL_PATH  = "model_path"
+LABEL_PATH  = "label_path"
+INPUT_SIZE  = "input_size"
+TYPE        = "type"
+
+# Define Result Dictionary Key
+DETECTION   = "detections"
+ID          = "id"
+LABEL       = "label"
+SCORE       = "score"
+XMIN        = "xmin"
+XMAX        = "xmax"
+YMIN        = "ymin"
+YMAX        = "ymax"
 
 
-class Classification():
+class Classification(IVIT_MODEL):
     
     def __init__(self, cfg):
+        """
+        Classification Object for IVIT-I-Xilinx
+        """
         self.cfg = cfg
+        self.init_status = False
 
-    def init_model(self):
-        
-        self.model = self.cfg[MODLES][XMODELS_CLASS][MODEL]
-        self.classes = self.cfg[MODLES][XMODELS_CLASS][CLASS]
-        self.input_size = self.cfg[MODLES][XMODELS_CLASS][INPUT_SIZE]
+    def init_model(self) -> bool:
+        """
+        Initialize Model.
+        - Return: 
+            - a bool value, which means is initialize successfuly or not.
+        """
+        # Parse Information From Config File
+        self.model_path = self.cfg[MODEL_PATH]
+        self.classes = load_txt(self.cfg[LABEL_PATH])
+        self.input_size = tuple(map(int, self.cfg[INPUT_SIZE].split(",")))
 
+        # Initialize XModel
         time_init_start = time.time()
-        ret = True
         try:
-            self.x = XMODEL(self.model, 'cnn')
+            self.x = XMODEL(self.model_path, 'cnn')
             self.x.init()
+            self.init_status = True
             logging.debug("Init cnn times = {:.4f} seconds".format(time.time() - time_init_start))
         except Exception as e:
-            ret = False
-        
-        return ret
+            handle_exception(e)
+            
+        return self.init_status
 
-    def inference(self, frame):
-        time1 = time.time()
+    def inference(self, frame) -> dict:
+        """
+        Do Inference.
+        - Argument:
+            - frame : frame is an image data
+        - Return:
+            - describe : a python dictionary include the result of detection
+            - example : 
+                {[{
+                    "id"        : 0,
+                    "label"     : cat,
+                    "score"     : None,
+                    "xmin"      : None
+                    "ymin"      : None
+                    "xmax"      : None
+                    "ymax"      : None
+                }]}
+        """
+        # Process Input Data
+        time_start = time.time()
         img = []
-        img.append(preprocess_fn(frame, self.input_size))
+        img.append(preprocess_fn(frame, self.input_size[1:]))
 
-
+        # Do Inference
         self.x.outputs = runDPU_(self.x, img[0:])
 
-        prediction = self.classes[np.argmax(self.x.outputs[0][0])]
+        # Parse Result
+        info = { DETECTION: list() }
+        temp_pattern        = self.get_dets_pattern()
+        temp_pattern[ID]    = np.argmax(self.x.outputs[0][0])
+        temp_pattern[LABEL] = self.classes[temp_pattern[ID]]
 
-        time2 = time.time()
-        time_total = time2 - time1
+        # Update info
+        info[DETECTION].append(temp_pattern)
+        logging.info(temp_pattern)
+
+        # Calculate During Time and Throughput
+        time_end = time.time()
+        time_total = time_end - time_start
         fps = 1 / time_total
-
-        logging.info("Throughput={:.2f} fps, total frames = {:.0f}, time={:.4f} seconds".format(fps, 1, time_total))
-
-        logging.info("Detection: {}".format(prediction))
-
-        ''' Put fps and pridict class on image '''
-        # frame = cv2.putText(frame, '{} fps: {:.4f}'.format(prediction, fps), (10, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2)
-
-        return frame
+        logging.info("Throughput = {:.2f} fps, time = {:.4f} seconds".format(fps, time_total))
+    
+        return info
--- ivit_i/obj/yolov3.py
+++ ivit_i/obj/yolov3.py
@@ -6,30 +6,52 @@
 import sys, os, copy, logging, time, random, colorsys
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import (
-	Lambda,
-)
-
-try:
-    import tensorflow as tf
-except:
-    pass
+from tensorflow.keras.layers import Lambda
 
 sys.path.append("..")
-from ivit_i.common.common import XMODEL, runDPU_
-from ivit_i.common.config_key_list import *
+from ivit_i.common.common import XMODEL, runDPU_, IVIT_MODEL
 from ivit_i.common.pre_post_proc import preprocess_fn
+from ivit_i.utils.parser import load_txt
+from ivit_i.utils.err_handler import handle_exception
 
-# As tensorflow lite doesn't support tf.size used in tf.meshgrid, 
-# we reimplemented a simple meshgrid function that use basic tf function.
+# Define Config Key
+TAG         = "tag"
+FRAMEWORK   = "framework"
+MODEL_PATH  = "model_path"
+LABEL_PATH  = "label_path"
+INPUT_SIZE  = "input_size"
+TYPE        = "type"
+ANCHORS     = "anchors"
+IOU         = "iou"
+NMS         = "nms"
+THRES       = "thres"
+BOX_MAX_NUM = "box_max_num"
+
+# Define Result Dictionary Key
+DETECTION   = "detections"
+ID          = "id"
+LABEL       = "label"
+SCORE       = "score"
+XMIN        = "xmin"
+XMAX        = "xmax"
+YMIN        = "ymin"
+YMAX        = "ymax"
+
+
 def _meshgrid(n_a, n_b):
+    """
+    As tensorflow lite doesn't support tf.size used in tf.meshgrid, 
+    we reimplemented a simple meshgrid function that use basic tf function.
+    """
     return [
         tf.reshape(tf.tile(tf.range(n_a), [n_b]), (n_b, n_a)),
         tf.reshape(tf.repeat(tf.range(n_b), n_a), (n_b, n_a))
     ]
 
 def yolo_boxes(pred, anchors, classes):
-    # pred: (batch_size, grid, grid, anchors, (x, y, w, h, obj, ...classes))
+    """
+    pred: (batch_size, grid, grid, anchors, (x, y, w, h, obj, ...classes))
+    """
     grid_size = tf.shape(pred)[1:3]
     box_xy, box_wh, objectness, class_probs = tf.split(
         pred, (2, 2, 1, classes), axis=-1)
@@ -54,7 +76,9 @@ def yolo_boxes(pred, anchors, classes):
     return bbox, objectness, class_probs, pred_box
 
 def yolo_nms(outputs, classes, max_output, iot_th, score_th):
-    # boxes, conf, type
+    """
+    boxes, conf, type
+    """
     b, c, t = [], [], []
 
     for o in outputs:
@@ -104,145 +128,162 @@ def yolo_nms(outputs, classes, max_output, iot_th, score_th):
 
 class YOLOV3_PATTERN(XMODEL):
 
-	def __init__(self, model, name, input_size, box_max_num, anchors, classed_num, iou, nms, conf):
-		super().__init__(model, name)
-		self.prediction = []
-		self.anchors = anchors
-		self.classes = classed_num
-		self.iou_th = iou
-		self.nms_th = nms
-		self.conf_th = conf
-		self.box_num = box_max_num
-		self.input_size = input_size
-
-	def sort_boxes(self):
+    def __init__(self, model, name, input_size, box_max_num, anchors, classed_num, iou, nms, thres):
         """
+        YOLOV3 PATTERN is made by Innodisk IPA Team 4 which inherit XMODEL
+        """
+        super().__init__(model, name)
         
+        self.prediction = []
+        self.anchors = anchors
+        self.classes = classed_num
+        self.iou_th = iou
+        self.nms_th = nms
+        self.conf_th = thres
+        self.box_num = box_max_num
+        self.input_size = input_size
+
+    def sort_boxes(self):
         """
-		if len(self.outputs) == 3:
-			''' yolo '''
-			boxes = np.array([[0, 1, 2], [0, 0, 0]], dtype=int)
-			boxes[1][0] = self.outputs[0].shape[1]
-			boxes[1][1] = self.outputs[1].shape[1]
-			boxes[1][2] = self.outputs[2].shape[1]
+        Sort bounding boxes.
+        
+        YOLO        : YOLO Layer is 3
+        YOLO-TINY   : YOLO Layer is 2
+        """
+        if len(self.outputs) == 3:
+            boxes = np.array([[0, 1, 2], [0, 0, 0]], dtype=int)
+            boxes[1][0] = self.outputs[0].shape[1]
+            boxes[1][1] = self.outputs[1].shape[1]
+            boxes[1][2] = self.outputs[2].shape[1]
 
-		elif len(self.outputs) == 2:
-			''' tiny yolo '''
-			boxes = np.array([[0, 1], [0, 0]], dtype=int)
-			boxes[1][0] = self.outputs[0].shape[1]
-			boxes[1][1] = self.outputs[1].shape[1]
+        elif len(self.outputs) == 2:
+            boxes = np.array([[0, 1], [0, 0]], dtype=int)
+            boxes[1][0] = self.outputs[0].shape[1]
+            boxes[1][1] = self.outputs[1].shape[1]
 
-		sorted = list(np.argsort(boxes[1:]))
-		sorted = sorted[0]
+        sorted = list(np.argsort(boxes[1:]))
+        sorted = sorted[0]
 
-		return sorted
+        return sorted
 
-	def pred_boxes(self):
-		if len(self.outputs) == 3:
-			''' yolo '''
-			a64 = np.reshape(self.outputs[self.sorted[2]], (1, int(self.input_size/8), int(self.input_size/8), 3, 5+self.classes))
-			a32 = np.reshape(self.outputs[self.sorted[1]], (1, int(self.input_size/16), int(self.input_size/16), 3, 5+self.classes))
-			a16 = np.reshape(self.outputs[self.sorted[0]], (1, int(self.input_size/32), int(self.input_size/32), 3, 5+self.classes))
+    def pred_boxes(self):
+        """
+        Parse information from bounding box.
 
-			b0 = Lambda(lambda x: yolo_boxes(x, self.anchors[:3], self.classes), name='yb0')(a64)
-			b1 = Lambda(lambda x: yolo_boxes(x, self.anchors[3:6], self.classes), name='yb1')(a32)
-			b2 = Lambda(lambda x: yolo_boxes(x, self.anchors[6:], self.classes), name='yb2')(a16)
+        YOLO        : YOLO Layer is 3
+        YOLO-TINY   : YOLO Layer is 2
+        """
+        if len(self.outputs) == 3:
+            a64 = np.reshape(self.outputs[self.sorted[2]], (1, int(self.input_size/8), int(self.input_size/8), 3, 5+self.classes))
+            a32 = np.reshape(self.outputs[self.sorted[1]], (1, int(self.input_size/16), int(self.input_size/16), 3, 5+self.classes))
+            a16 = np.reshape(self.outputs[self.sorted[0]], (1, int(self.input_size/32), int(self.input_size/32), 3, 5+self.classes))
 
-			self.prediction = Lambda(
-				lambda x: yolo_nms(x, self.classes, self.box_num, self.iou_th, self.nms_th),
-				name='yolo_nms')((b0[:3], b1[:3], b2[:3]))
+            b0 = Lambda(lambda x: yolo_boxes(x, self.anchors[:3], self.classes), name='yb0')(a64)
+            b1 = Lambda(lambda x: yolo_boxes(x, self.anchors[3:6], self.classes), name='yb1')(a32)
+            b2 = Lambda(lambda x: yolo_boxes(x, self.anchors[6:], self.classes), name='yb2')(a16)
 
-			boxes, scores, classes, nums = self.prediction
-			return np.array(boxes), np.array(scores), np.array(classes), np.array(nums)
+            self.prediction = Lambda(
+                lambda x: yolo_nms(x, self.classes, self.box_num, self.iou_th, self.nms_th),
+                name='yolo_nms')((b0[:3], b1[:3], b2[:3]))
 
-		elif len(self.outputs) == 2:
-			''' tiny yolo '''
-			o_13 = self.outputs[0]
-			o_26 = self.outputs[1]
+            boxes, scores, classes, nums = self.prediction
+            return np.array(boxes), np.array(scores), np.array(classes), np.array(nums)
 
-			a32 = np.reshape(o_13, (1, int(self.input_size/32), int(self.input_size/32), 3, 5+self.classes))
-			a16 = np.reshape(o_26, (1, int(self.input_size/16), int(self.input_size/16), 3, 5+self.classes))
+        elif len(self.outputs) == 2:
+            o_13 = self.outputs[0]
+            o_26 = self.outputs[1]
 
-			b1 = Lambda(lambda x: yolo_boxes(x, self.anchors[3:6], self.classes), name='yb0')(a32)
-			b2 = Lambda(lambda x: yolo_boxes(x, self.anchors[0:3], self.classes), name='yb1')(a16)
+            a32 = np.reshape(o_13, (1, int(self.input_size/32), int(self.input_size/32), 3, 5+self.classes))
+            a16 = np.reshape(o_26, (1, int(self.input_size/16), int(self.input_size/16), 3, 5+self.classes))
 
-			self.prediction =  Lambda(
-				lambda x: yolo_nms(x, self.classes, self.box_num, self.iou_th, self.nms_th),
-				name='yolo_nms')((b1[:3], b2[:3]))
+            b1 = Lambda(lambda x: yolo_boxes(x, self.anchors[3:6], self.classes), name='yb0')(a32)
+            b2 = Lambda(lambda x: yolo_boxes(x, self.anchors[0:3], self.classes), name='yb1')(a16)
 
-			boxes, scores, classes, nums = self.prediction
-			return np.array(boxes), np.array(scores), np.array(classes), np.array(nums)
+            self.prediction =  Lambda(
+                lambda x: yolo_nms(x, self.classes, self.box_num, self.iou_th, self.nms_th),
+                name='yolo_nms')((b1[:3], b2[:3]))
 
-			# a32 = np.reshape(self.outputs[self.sorted[1]], (1, int(self.input_size/16), int(self.input_size/16), 3, 5+self.classes))
-			# a16 = np.reshape(self.outputs[self.sorted[0]], (1, int(self.input_size/32), int(self.input_size/32), 3, 5+self.classes))
+            boxes, scores, classes, nums = self.prediction
 
-			# b0 = Lambda(lambda x: yolo_boxes(x, self.anchors[0:3], self.classes), name='yb1')(a32)
-			# b1 = Lambda(lambda x: yolo_boxes(x, self.anchors[3:], self.classes), name='yb2')(a16)
+            return np.array(boxes), np.array(scores), np.array(classes), np.array(nums)
 
-			# self.prediction = Lambda(
-			# 		lambda x: yolo_nms(x, self.classes, self.box_num, self.iou_th, self.nms_th),
-			# 		name='yolo_nms')((b0[:3], b1[:3]))
-
-class YOLOv3():
+class YOLOv3(IVIT_MODEL):
 
     def __init__(self, cfg):
+        """
+        YOLOv3 Object for IVIT-I-Xilinx
+        """
         self.cfg    = cfg
-
-        self.run_model  = None
-        self.output     = None
-        self.get_frame  = None
-
-        self.f_first_run_model    = True
-        self.f_first_output       = True
-        self.f_first_get_frame    = True
+        self.init_status = False
 
     def init_model(self):
-        ''' Get config from cfg '''
-        self.iou = float(self.cfg[MODLES][XMODELS_OBJ][IOU])
-        self.nms = float(self.cfg[MODLES][XMODELS_OBJ][NMS])
-        self.conf = float(self.cfg[MODLES][XMODELS_OBJ][CONF])
-        self.box_max_num = int(self.cfg[MODLES][XMODELS_OBJ][BOX_MAX_NUM])
-        raw_anchors = self.cfg[MODLES][XMODELS_OBJ][ANCHORS]
+        """
+        Initialize Model.
+        - Return: 
+            - a bool value, which means is initialize successfuly or not.
+        """
+        
+        # Get information from config
+        self.model_path = self.cfg[MODEL_PATH]
+        self.classes = load_txt(self.cfg[LABEL_PATH])
+        self.input_size = tuple(map(int, self.cfg[INPUT_SIZE].split(",")))[1:]        
+        self.classed_num = len(self.classes)
 
-        self.model = self.cfg[MODLES][XMODELS_OBJ][MODEL]
-        self.classes = self.cfg[MODLES][XMODELS_OBJ][CLASS]
-        self.input_size = self.cfg[MODLES][XMODELS_OBJ][INPUT_SIZE]
+        self.iou = float(self.cfg[IOU])
+        self.nms = float(self.cfg[NMS])
+        self.thres = float(self.cfg[THRES])
+        self.box_max_num = int(self.cfg[BOX_MAX_NUM])
+        raw_anchors = self.cfg[ANCHORS]
 
-        self.label_classes = len(self.classes)
-
-        ''' Set anchors '''
-        rows = int(len(raw_anchors) / 2)
-        cols = 2
-
+        # Set anchors
+        rows, cols = int(len(raw_anchors) / 2), 2
         self.anchors = np.zeros(shape=(rows, cols), dtype=float)
         for i in range(rows):
             for j in range(cols):
                 self.anchors[i][j] = int(raw_anchors[cols * i + j]) / self.input_size[0]
 
-        # random color for predict box
-        self.color_list = []
-        for i in range(len(self.classes)):
-            h, s, l = random.random(), 0.5 + random.random() / 2.0, 0.4 + random.random() / 5.0
-            r, g, b = [int(256 * i) for i in colorsys.hls_to_rgb(h, l, s)]
-            self.color_list.append([r, g, b])
-
-        time_init_start = time.time()
-        ret = True
-        
+        # Initialize YOLO Model        
         try:
-            self.x = YOLOV3_PATTERN(self.model, 'yolov3', self.input_size[0], self.box_max_num, self.anchors, self.label_classes, self.iou, self.nms, self.conf)
+            time_init = time.time()
+            self.x = YOLOV3_PATTERN(    model=self.model_path, 
+                                        name='yolov3', 
+                                        input_size=self.input_size[0], 
+                                        box_max_num=self.box_max_num, 
+                                        anchors=self.anchors, 
+                                        classed_num=self.classed_num, 
+                                        iou=self.iou, 
+                                        nms=self.nms, 
+                                        thres=self.thres  )
             self.x.init()
-            logging.debug("Init yolov3 times = {:.4f} seconds".format(time.time() - time_init_start))
+            self.init_status = True
+            logging.debug("Init {} times = {:.4f} seconds".format('yolov3', time.time() - time_init))
         
         except Exception as e:
-            ret = False
-            logging.error(e)
+            handle_exception(e)
             
-        return ret
+        return self.init_status
 
     def inference(self, frame):
+        """
+        Do Inference.
+        - Argument:
+            - frame : frame is an image data
+        - Return:
+            - describe : a python dictionary include the result of detection
+            - example : 
+                {[{
+                    "id"        : 0,
+                    "score"     : 0.9123,
+                    "label"     : cat,
+                    "xmin"      : 32
+                    "ymin"      : 435
+                    "xmax"      : 42
+                    "ymax"      : 654
+                }]}
+        """
 
-        time1 = time.time()
+        # Process Input Data
+        time_start = time.time()
         img = []
         image = preprocess_fn(frame, self.input_size)
         img.append(image)
@@ -250,30 +291,38 @@ class YOLOv3():
         # Do Inference
         self.x.outputs = runDPU_(self.x, img[0:])
 
+        # Parse information from Bounding Box
         time_pred_box = time.time()
-        
-        # Sort Bounding Box
         self.x.sorted = self.x.sort_boxes()
-        # self.x.prediction = self.x.pred_boxes()
+        self.p_boxes, self.p_scores, self.p_classes, self.p_nums = self.x.pred_boxes()
         logging.debug("bb output times = {:.4f} seconds".format(time.time() - time_pred_box))
 
-        # Parse Bounding Box
-        self.p_boxes, self.p_scores, self.p_classes, self.p_nums = self.x.pred_boxes()
-
-        time2 = time.time()
-        time_total = time2 - time1
-
-        fps = 1 / time_total
-
-        logging.info(" Throughput={:.2f} fps, total frames = {:.0f}, time = {:.4f} seconds".format(fps, 1, time_total))
-
-        logging.info(' Detections: {}'.format( len(self.p_nums) ))
-
+        # Parse the result
+        logging.info("Detections: {}".format( len(self.p_nums) ))
+        info = { DETECTION: list() }
         for i in range(self.p_nums[0]):
-            logging.info('\t{}, {}, {}'.format(self.classes[int(self.p_classes[0][i])],
-												np.array(self.p_scores[0][i]),
-												np.array(self.p_boxes[0][i])))
             
-            # frame = draw_outputs(frame, (self.p_boxes, self.p_scores, self.p_classes, self.p_nums), self.classes, i, self.color_list[int(self.p_classes[0][i])], fps)
+            # Add Info and Show Result
+            temp_pattern = self.get_dets_pattern()
+            temp_pattern[ID]      = int(self.p_classes[0][i])
+            temp_pattern[SCORE]   = float(self.p_scores[0][i])
+            temp_pattern[LABEL]   = self.classes[int(self.p_classes[0][i])]
 
-        return frame
+            # Rescale BBOX to image size
+            bbox = np.array(self.p_boxes[0][i])     # x1, y1, x2, y2
+            wh = np.flip(frame.shape[0:2])          # hwc to wh
+            temp_pattern[XMIN], temp_pattern[YMIN] = np.array((bbox[0:2] * wh)).astype(np.int32)
+            temp_pattern[XMAX], temp_pattern[YMAX] = np.array((bbox[2:4] * wh)).astype(np.int32)
+
+            # Update info
+            info[DETECTION].append( temp_pattern )
+            logging.info("\t[{}] {}".format(i, temp_pattern))
+        
+        # Calculate During Time and Throughput
+        time_end = time.time()
+        time_total = time_end - time_start
+        fps = 1 / time_total
+        
+        logging.info("Throughput = {:.2f} fps, time = {:.4f} seconds".format(fps, time_total))
+
+        return info
