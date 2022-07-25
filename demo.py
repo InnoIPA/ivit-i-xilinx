da03a78795779d377da3961355c97bb477b34880
HuiJu1218 <ushueiru4567mail.com>
2022-07-24 23:50:01 -0700

    feat: add application

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 import argparse
 import time
 import numpy as np
@@ -13,8 +14,12 @@ from ivit_i.utils.logger import config_logger
 from ivit_i.utils.parser import load_json
 from ivit_i.common import api
 from ivit_i.common.pipeline import Source
-from ivit_i.utils.drawing_tools import Draw
+from ivit_i.utils.drawing_tools import Draw, get_palette
 from ivit_i.utils.err_handler import handle_exception
+
+from ivit_i.app.handler import get_application
+
+CV_WIN  =   "Test"
 
 # legacy
 # from mod.predictor import PREDICTOR
@@ -32,29 +37,68 @@ def main(args):
 
     logging.debug(total_conf)
 
+    # Load iVIT-I API
     try:
         logging.debug("Loading ivit_i API")
         trg = api.get(total_conf)
         draw = Draw()
+        palette = get_palette(total_conf)
     except Exception as e:
         handle_exception(error=e, title="Could not get ivit-i API", exit=True)
 
+    # Load Model
     try:
         ret = trg.init_model()
-        
     except Exception as e:
         handle_exception(error=e, title="Could not load AI model", exit=True)
 
+    # Get source object
     src = Source(total_conf['source'], total_conf['source_type'])
 
+    # Get Application
+    has_app=False
+    try:
+        application = get_application(total_conf)
+        has_app = False if application == None else True
+
+        # Setup parameter if needed
+        app_info = total_conf["application"]
+
+        # Area detection: point_points
+        if "area" in app_info["name"]:
+            key = "area_points"
+            if not key in app_info: 
+                application.set_area(pnts=None, frame=src.get_first_frame())
+            else: 
+                application.set_area(pnts=app_info[key])   
+
+    except Exception as e:
+        handle_exception(error=e, title="Could not load application ... set app to None", exit=False)
+        has_app=False
+
+    # Main Loop
     try:
         while True:
             
             ret_frame, frame = src.get_frame()
 
-            org_frame = frame.copy()
+            draw_frame = frame.copy()
 
-            frame = trg.inference(org_frame)
+            info = trg.inference(frame)
+
+            if not has_app:
+                draw_frame = draw.draw_detections(draw_frame, info, palette, total_conf)
+            else:
+                draw_frame = application(draw_frame, info)
+
+            # show the results            
+            cv2.imshow(CV_WIN, draw_frame)
+            key = cv2.waitKey(1)
+            if key in [ ord('q'), ord('Q'), 27 ]:
+                break 
+
+        src.release()
+
     except KeyError as e:
         pass
 
