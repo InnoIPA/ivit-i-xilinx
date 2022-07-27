71c32c6afc883190c9e2294ba80b5de3768d723b
HuiJu1218 <ushueiru4567mail.com>
2022-07-27 03:05:29 -0700

    feat: replace to mobilenet ( original is resnet )

--- task/classification_sample/download_model.sh
+++ task/classification_sample/download_model.sh
@@ -1,9 +1,15 @@
 # Download Model
+printf "\n"
+printf "# Download Model \n"
 
 # Defince
-URL="https://www.xilinx.com/bin/public/openDownload?filename=resnet50_tf2-zcu102_zcu104_kv260-r1.4.0.tar.gz"
-FILE="resnet50_tf2-zcu102_zcu104_kv260-r1.4.0.tar.gz"
-FOLD="`dirname ${0}`/model"
+URL="https://www.xilinx.com/bin/public/openDownload?filename=mobilenet_1_0_224_tf2-zcu102_zcu104_kv260-r1.4.1.tar.gz"
+FILE="mobilenet_1_0_224_tf2-zcu102_zcu104_kv260-r1.4.1.tar.gz"
+ROOT=`dirname ${0}`
+FOLD="model"
+
+# Move to the target place
+cd ${ROOT}
 
 # Download from Model Zoo
 wget ${URL} -O ${FILE}
@@ -11,3 +17,6 @@ wget ${URL} -O ${FILE}
 # Create model and extract model into it
 mkdir ${FOLD}
 tar -zxvf ${FILE} -C ${FOLD} --strip-components=1
+
+# Remove ZIP
+rm ${FILE}
