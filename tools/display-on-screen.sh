d0642400e471534e9c9c8043449d7c3a68570fea
MaxChangInnodisk <max_chang@innodisk.com>
2023-05-22 03:05:31 -0700

    feat: remove tools

--- tools/display-on-screen.sh
+++
@@ -1,5 +0,0 @@
-gst-launch-1.0 -v \
-v4l2src device=/dev/video0 \
-! video/x-raw, width=1920, height=1080 \
-! videoconvert \
-! kmssink bus-id=fd4a0000.zynqmp-display fullscreen-overlay=1 sync=false
--- tools/enable_display.sh
+++
@@ -1,8 +0,0 @@
-cat << EOF
-
-After Run this script, the screen should display rainbow lik the fakesink output.
-
-EOF
-
-modetest -M xlnx -D fd4a0000.zynqmp-display -s 43@41:1920x1080@AR24
-
